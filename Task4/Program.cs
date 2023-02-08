2b2872782120791bab836e982af4c12982acf4f3
Roman Linkov <roman.linkov@vk.com>
2023-02-08 19:32:49 +0300

    Сделана вторая программа, см. README

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Салам алейкум, брат!");
+Console.WriteLine("Введи вервое целое число, брат:");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе целое число, брат:");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи третье целое число, брат:");
+int NumberC = Convert.ToInt32(Console.ReadLine());
+
+int max = NumberA;
+
+if (max < NumberB) 
+{
+    max = NumberB;
+}
+if (max < NumberC) 
+{
+    max = NumberC;
+}
+
+Console.Write("Наибольшее из трёх чисел: ");
+    Console.WriteLine(max);
