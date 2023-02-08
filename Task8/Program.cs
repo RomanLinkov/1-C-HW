03151d2a68e3d8293f54fa5ed9677f997508c466
Roman Linkov <roman.linkov@vk.com>
2023-02-08 20:04:00 +0300

    Сделана чётвертая программа, выводящая все чётные числа меньшие или равные введённому

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Салам алейкум, брат!");
+Console.WriteLine("Введи натуральное число, брат:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+int N1 = N-1;
+Console.Write("Все чётные натуральные числа, меньшие или равные твоему числу: ");
+
+while (count <= N)
+{
+    Console.Write(count);
+    if (count<N1) { Console.Write(", ");}
+    count=count+2;
+}
+Console.WriteLine();
