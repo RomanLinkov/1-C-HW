8f6a3554ceadd4e114a7b4a2e426a72cd4e148bc
Roman Linkov <roman.linkov@vk.com>
2023-02-08 17:54:05 +0300

    Написана первая программа

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Салам алейкум, брат!");
+Console.WriteLine("Введи вервое целое число, брат:");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе целое число, брат:");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+
+if(NumberA > NumberB)
+{
+    Console.Write("Наибольшее число: ", NumberA);
+    Console.WriteLine(NumberA);
+}
+else
+{
+    Console.Write("Наибольшее число: ");
+    Console.WriteLine(NumberB);
+}
